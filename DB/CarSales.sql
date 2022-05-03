46bb04dbda724e721d76cbb01f0235a4ce16e01d
Eduardo Xuyá <eduardo.xuya@gt.ey.com>
2022-05-02 23:53:47 -0600

    Add method for add new cars (Added template html and query)

---
+++ DB/CarSales.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE CarSales
+USE CarSales
+CREATE TABLE [dbo].[TblCars](
+	[ID] [int] NOT NULL,
+	[Name] [varchar](100) NULL,
+	[Year] [int] NOT NULL,
+	[Price] [float] NOT NULL)
+
+INSERT INTO TblCars VALUES (1, 'Toyota Camry', 2018, 2000)
+INSERT INTO TblCars VALUES (2, 'Honda Civic', 2019, 2200)
+INSERT INTO TblCars VALUES (3, 'Chevrolet Silverado', 2017, 1800)
+INSERT INTO TblCars VALUES (4, 'Ford F-150', 2020, 2500)
+INSERT INTO TblCars VALUES (5, 'Nissan Altima', 2021, 3000)
--- carsales.py
+++ carsales.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template, jsonify)
+from flask import (Flask, render_template, jsonify, request, redirect)
 import pyodbc
 
 carsales = Flask(__name__)
@@ -12,7 +12,7 @@ def connection():
     conn = pyodbc.connect(cstr)
     return conn
 
-@carsales.route("/")
+@carsales.route("/",methods = ['GET'])
 def main():
     data=[]
     cars = []
@@ -26,6 +26,22 @@ def main():
     cars = cars
     #return jsonify(cars) This is for return data in JSON to consume in other app, use POSTMAN for consume
     return render_template("carslist.html", cars = cars)
+
+@carsales.route("/addcar", methods = ['GET','POST'])
+def addcar():
+    if request.method == 'GET':
+        return render_template("addcar.html")
+    if request.method == 'POST':
+        id = int(request.form["id"])
+        name = request.form["name"]
+        year = int(request.form["year"])
+        price = float(request.form["price"])
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO dbo.TblCars (id, name, year, price) VALUES (?, ?, ?, ?)", id, name, year, price)
+        conn.commit()
+        conn.close()
+        return redirect('/')
 
 if(__name__ == "__main__"):
     carsales.run()
